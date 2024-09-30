d75fbc3f0306ecfc32f76e90ee1f2684e97a4b83
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-30 09:06:12 +0200

    Ran pyink on branch solution

--- main_test.py
+++ main_test.py
@@ -1,5 +1,6 @@
 from main import even_numbers_generator
 
+
 def test_even_numbers_generator():
     even_numbers = list(even_numbers_generator(15))
     assert even_numbers == [2, 4, 6, 8, 10, 12, 14]
